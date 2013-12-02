c609c3c22a6498873508ef08ad27d3b4a8043f03
Remy Jardinet <remy.jardinet@gmail.com>
2013-12-02 02:09:38 +0100

    Ajout de la base API

---
+++ src/Pouzor/Bundle/LolStatBundle/Controller/ApiController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Pouzor\Bundle\LolStatBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+*
+* @Route("/API")
+*
+*/
+class ApiController extends Controller
+{
+
+
+
+	/**
+	*
+	*
+	*/
+	private function getData($url)
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+    	$header = array("X-Mashape-Authorization: ".$this->container->getParameter('mashape_key'));
+    	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    	$output = curl_exec($ch);
+    	curl_close($ch);
+    	return $output;
+	}
+
+
+	/**
+	*
+	* @Template()
+	* @Route("/getAggregatedStats")
+	*
+	*/
+	public function getAggregatedStatsAction() {
+
+
+		$data = $this->getData("https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getAggregatedStats/"."38883435");
+
+		print_r($data);die();
+
+		return array();
+	}
+}
