8c6ec3b7118dd05f535529a818b83db8fb4179fb
Jardinet <remy.jardinet@gmail.com>
2013-12-09 15:41:57 +0100

    insight

--- src/Pouzor/Bundle/LolStatBundle/DependencyInjection/Configuration.php
+++ src/Pouzor/Bundle/LolStatBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pouzor_lol_stat');
+        $treeBuilder->root('pouzor_lol_stat');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
