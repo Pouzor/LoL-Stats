d5b826d47fa0673f30ab10c4d8bd2ae6d0dc56c1
Jardinet <remy.jardinet@gmail.com>
2013-12-09 15:49:06 +0100

    insight

--- src/Pouzor/Bundle/LolStatBundle/Controller/IndexController.php
+++ src/Pouzor/Bundle/LolStatBundle/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace Pouzor\Bundle\LolStatBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class IndexController extends Controller
 {
@@ -12,12 +13,13 @@ class IndexController extends Controller
 	*
 	* @Template()
 	* @Route("/", name="homepage")
+    * @Method({"GET"})
 	*
 	*/
 	public function indexAction() {
 
 		$summoners = $this->getDoctrine()->getRepository("PouzorLolStatBundle:User")->findAll();
 
-		return array("summoners" => $summoners);		
+		return array("summoners" => $summoners);
 	}
 }
