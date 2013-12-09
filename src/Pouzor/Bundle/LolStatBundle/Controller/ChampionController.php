2f3a5834b1ada8561d3d47c7f3d929466345b897
Jardinet <remy.jardinet@gmail.com>
2013-12-09 15:44:44 +0100

    insight

--- src/Pouzor/Bundle/LolStatBundle/Controller/ChampionController.php
+++ src/Pouzor/Bundle/LolStatBundle/Controller/ChampionController.php
@@ -5,6 +5,7 @@ namespace Pouzor\Bundle\LolStatBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ChampionController extends Controller
 {
