736a5a05b4fe6fa90ebbafe4e076d7d75e00006f
Remy Jardinet <remy.jardinet@gmail.com>
2013-12-21 19:39:40 +0100

    debug

--- src/Pouzor/Bundle/LolStatBundle/Command/FetchMatchFromAPICommand.php
+++ src/Pouzor/Bundle/LolStatBundle/Command/FetchMatchFromAPICommand.php
@@ -110,7 +110,7 @@ private function get($url)
 
           foreach ($g["statistics"]["array"] as $d) {
                 //DEBUG
-                //$output->writeln($d["statType"]." - ".$d["value"]);
+                $output->writeln($d["statType"]." - ".$d["value"]);
             switch ($d["statType"]) {
                 case "BARRACKS_KILLED":
                 break;
