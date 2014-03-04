a9d8f59796c0888702b1c3e2332985d25e5b22ed
Rémy Jardinet <remy.jardinet.ext@francetv.fr>
2014-03-04 15:56:47 +0100

    Corrections diverses

--- src/Pouzor/Bundle/LolStatBundle/Twig/LeagueExtension.php
+++ src/Pouzor/Bundle/LolStatBundle/Twig/LeagueExtension.php
@@ -9,7 +9,7 @@ class LeagueExtension extends \Twig_Extension {
     public function getFunctions()
     {
         return array(
-            'getLeague' => new \Twig_SimpleFunction($this, 'getLeague')
+            'getLeague' => new \Twig_Function($this, 'getLeague')
             );
     }
 
